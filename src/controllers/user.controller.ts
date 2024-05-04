64c93c42865d08a5bf20b748dee610b09277bcf2
vancong <108638136+NguyenCong2806@users.noreply.github.com>
2024-05-04 19:30:56 +0700

    upcode

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -9,7 +9,7 @@ import {
   Put,
   Query,
   Res,
-  UseGuards,
+  // UseGuards,
 } from '@nestjs/common';
 import { Response } from 'express';
 import { User } from './../models/database/User';
@@ -18,14 +18,14 @@ import Paginations from 'src/models/BaseModel/Paginations';
 import { UpdateTodoDto } from '../models/viewmodel/user/UpdateUserDto';
 import { CreateTodoDto } from '../models/viewmodel/user/CreateUserDto';
 import SerachPara from 'src/models/BaseModel/SerachPara';
-import { AuthGuard } from 'src/Guard/auth.guard';
+//import { AuthGuard } from 'src/Guard/auth.guard';
 import { Roles } from 'src/decorator/roles.decorator';
 import * as argon2 from 'argon2';
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { IUserService } from 'src/services/user/IUserService';
 
 @Controller('user')
-@UseGuards(AuthGuard)
+//@UseGuards(AuthGuard)
 @Roles('admin', 'member')
 export class UsersController {
   constructor(private readonly usersService: UserService) {}
--- src/models/database/User.ts
+++ src/models/database/User.ts
@@ -17,6 +17,10 @@ export class User extends BaseEntity {
 
   @Prop({ required: true })
   password: string;
+  @Prop()
+  site: string;
+  @Prop()
+  location: number;
 }
 
 export const UserSchema = SchemaFactory.createForClass(User);
